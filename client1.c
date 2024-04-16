70b23bd50529d556c363a8f1f811649d9b696e6b
Aaro Mäkinen <aaro@tuxera.com>
2024-04-16 22:07:45 +0300

    Add reading for all the ports and present in json format

--- client1.c
+++ client1.c
@@ -2,25 +2,40 @@
 #include <string.h>
 #include <unistd.h>
 #include "socket.h"
+#include <math.h>
+#include <sys/time.h>
 
 #define ip_addr "172.17.0.1"
 #define buffer_len 100
 
+long long time_in_ms(void) {
+    struct timeval tv;
 
+    gettimeofday(&tv,NULL);
+    return (((long long)tv.tv_sec)*1000)+(tv.tv_usec/1000);
+}
 
 int main(int argc, char *argv[]) {
 	char *msg;
+	char val[3][5];
 	char buffer[buffer_len];
-	int socket_fd;
+	int socket_fd[4], i;
+	int ports[4] = { 4000, 4001, 4002, 4003 };
 
-	socket_fd = init_socket(ip_addr, 4001);
-	if (socket_fd == -1)
-		return -1;
+	for (i = 1; i < 4; i++) {
+		socket_fd[i] = init_socket(ip_addr, ports[i]);
+		if (socket_fd[i] == -1)
+			return -1;
+	}
 	while(1) {
-		msg = read_from_port(socket_fd, 4001, buffer,
-				buffer_len);
+		for (i = 1; i < 4; i++) {
+			msg = read_from_port(socket_fd[i], ports[i], buffer,
+					buffer_len);
+			memcpy(val[i-1], msg, 5);
+		}
+
+		printf("{\"timestamp\": %lld, \"out1\": \"%s\", \"out2\": \"%s\", \"out3\": \"%s\"}\n", time_in_ms(), val[0], val[1], val[2]);
 		usleep(100000);
-		printf("value: %s\n", msg);
 	}
 	return 0;
 }
--- socket.c
+++ socket.c
@@ -14,7 +14,7 @@ int init_socket(char* ip_addr, int port) {
 	}
 	server_addr.sin_family = AF_INET;
 	server_addr.sin_port = htons(port);
-	server_addr.sin_addr.s_addr = inet_addr("172.17.0.1");
+	server_addr.sin_addr.s_addr = inet_addr(ip_addr);
 	if(connect(socket_fd, (struct sockaddr*)&server_addr,
 			sizeof(server_addr)) < 0){
 	    printf("Unable to connect\n");
@@ -22,6 +22,7 @@ int init_socket(char* ip_addr, int port) {
 	}
 	return socket_fd;
 }
+
 char* read_from_port(int socket_fd, int port, char* msg, size_t len) {
 	size_t index;
 	int i;
@@ -29,8 +30,8 @@ char* read_from_port(int socket_fd, int port, char* msg, size_t len) {
 
 	memset(msg,'\0', len);
 	msg[99] = '\0';
-	if(recv(socket_fd, msg, len-1, 0) < 0){
-		value = "-";
+	if(recv(socket_fd, msg, len-1, MSG_DONTWAIT) < 0){
+		value = "--";
 	}
 	else {
 		/* We only consider value fully sent if it ends with new
@@ -38,7 +39,6 @@ char* read_from_port(int socket_fd, int port, char* msg, size_t len) {
 		 * find the second to last new line or the beginning of
 		 * the buffer to get the last value
 		 */
-		printf("Server's response: %s\n", msg);
 		index = strrchr(msg,'\n') - msg;
 		msg[index] = '\0';
 		for (i = index - 1; i >= 0; i--){
